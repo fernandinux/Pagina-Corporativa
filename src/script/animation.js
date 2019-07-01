f4861f0dea086a0b002e67f4ae1de25c28f5437f
Flor Ballinas <flor_ballinas@hotmail.com>
2019-06-30 20:33:33 -0500

    animaciones de la página

---
+++ src/script/animation.js
@@ -0,0 +1,33 @@
+// Scroll Reveal
+// Animación a navbar
+window.sr = ScrollReveal();
+sr.reveal('.nav', {
+  distance: '50px',
+  duration: 1000,
+  origin: 'top'
+});
+
+// Animación a título
+sr.reveal('h1', {
+  distance: '200px',
+  duration: 2000,
+  origin: 'right'
+});
+
+// Animación al texto central
+sr.reveal('.txt', {
+  distance: '200px',
+  duration: 2000,
+  origin: 'top',
+});
+
+// Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
