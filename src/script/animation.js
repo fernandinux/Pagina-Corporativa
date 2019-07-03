fd165d2a62729cc0a0554d15aa5376b3735f994a
Flor Ballinas <flor_ballinas@hotmail.com>
2019-07-03 01:27:45 -0500

    añadiendo nuevos estilos, sección de te detalles, y formulario de prueba.

--- src/script/animation.js
+++ src/script/animation.js
@@ -25,7 +25,6 @@ sr.reveal('.txt', {
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
-
     document.querySelector(this.getAttribute('href')).scrollIntoView({
       behavior: 'smooth'
     });
